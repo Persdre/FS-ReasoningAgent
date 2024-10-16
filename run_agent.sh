9faca362d53d577cc1b230a14fe69f1f07e4de2b
Qian Wang <persdre@gmail.com>
2024-10-16 17:09:38 +0800

    update commands

--- run_agent.sh
+++ run_agent.sh
@@ -1,17 +1,17 @@
 # btc
 ## o1-mini bull
-python -u run_agent.py --model o1-mini --dataset btc --starting_date 2023-10-01 --ending_date 2023-12-01 &>updated_reasoning/btc-bull-o1-mini-reasoning.out 2>&1
+python -u run_agent.py --model o1-mini --dataset btc --starting_date 2024-01-24 --ending_date 2024-03-13 &>updated_reasoning/btc-bull-o1-mini-reasoning.out 2>&1
 
 ## o1-mini bear
 python -u run_agent.py --model o1-mini --dataset btc --starting_date 2024-05-21 --ending_date 2024-07-08 &>updated_reasoning/btc-bear-o1-mini-reasoning.out 2>&1
 
 ## 4o bull
-python -u run_agent.py --model gpt-4o --dataset btc --starting_date 2023-10-01 --ending_date 2023-12-01 &>updated_reasoning/btc-bull-4o-reasoning.out 2>&1
+python -u run_agent.py --model gpt-4o --dataset btc --starting_date 2024-01-24 --ending_date 2024-03-13 &>updated_reasoning/btc-bull-4o-reasoning.out 2>&1
 ## 4o bear
 python -u run_agent.py --model gpt-4o --dataset btc --starting_date 2024-05-21 --ending_date 2024-07-08 &>updated_reasoning/btc-bear-4o-reasoning.out 2>&1  
 
 ## 3.5 bull
-python -u run_agent.py --model gpt-3.5-turbo --dataset btc --starting_date 2023-10-01 --ending_date 2023-12-01 &>updated_reasoning/btc-bull-3.5-reasoning.out 2>&1
+python -u run_agent.py --model gpt-3.5-turbo --dataset btc --starting_date 2024-01-24 --ending_date 2024-03-13 &>updated_reasoning/btc-bull-3.5-reasoning.out 2>&1
 ## 3.5 bear
 python -u run_agent.py --model gpt-3.5-turbo --dataset btc --starting_date 2024-05-21 --ending_date 2024-07-08 &>updated_reasoning/btc-bear-3.5-reasoning.out 2>&1
 
